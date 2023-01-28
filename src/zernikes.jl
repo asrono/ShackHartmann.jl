3ff584fc4e4a2b2a578e1d75075e30a6f4eb76fb
asrono <nielsbuijssen@hotmail.com>
2023-01-28 22:19:14 +0100

    Zernike polynomials definition

--- src/zernikes.jl
+++ src/zernikes.jl
@@ -6,6 +6,18 @@
 # Z_n^-m(r,theta) = R_n^m(r)*sin(m*theta) if m < 0
 # NOTE: theta is measured clockwise from the y-axis. This is consistent with aberration theory, but different from conventional mathematical definition
 
-function R(n,m,r)
-    return 0
+function R(r::Real; n::Int,m::Int)::Float64
+    @assert n>=0 "n has to be non-negative!"
+    @assert abs(m)<=n "n has to be at least abs(m)"
+    @assert (n - m) % 2 == 0 "In your input, n - m has to be even!"
+    @assert 0<=r<=1 "Ensure r is restricted to the unit circle (0<=r<=1)" 
+
+    output = 0.0
+    for l in 0:(n-m)÷2
+        nom = (-1)^l*factorial(n-l)
+        den = factorial(l)*factorial((n+m)÷2-l)*factorial((n-m)÷2-l)
+        output += nom/den*r^(n-2*l)
+    end
+
+    return output
 end
--- test/testZernikes.jl
+++ test/testZernikes.jl
@@ -1,6 +1,17 @@
 using ShackHartmann
 using Test
 
-@testset "Test zernike generation" begin
-    @test ShackHartmann.R(1,1,2) == 0
+@testset "Zernike radial function" begin
+    @test ShackHartmann.R(0.5; n=1, m=1) isa Float64
+    @test_throws AssertionError("n has to be non-negative!") ShackHartmann.R(0.0; n=-1, m=1)
+    @test_throws AssertionError("n has to be at least abs(m)") ShackHartmann.R(0.0; n=3, m=4)
+    @test_throws AssertionError("In your input, n - m has to be even!") ShackHartmann.R(0; n=2, m=1) 
+    @test_throws AssertionError("Ensure r is restricted to the unit circle (0<=r<=1)") ShackHartmann.R(-0.1; n=3, m=1)
+    @test_throws AssertionError("Ensure r is restricted to the unit circle (0<=r<=1)") ShackHartmann.R(1.1; n=3, m=3)
+
+    @test ShackHartmann.R(1; n=0, m=0) == 1.0
+    @test ShackHartmann.R(1; n=3, m=3) == 1.0
+    @test ShackHartmann.R(0; n=2, m=0) == -1.0
+
+    @test ShackHartmann.R.([0., 1.0];  n=0, m=0) = [1.0, 1.0]
 end
