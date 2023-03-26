c9c6bb257d728bb31c498c8619ba39060319c89f
asrono <nielsbuijssen@hotmail.com>
2023-03-26 16:36:15 +0200

    3D surface plots

--- examples/simple_example.jl
+++ examples/simple_example.jl
@@ -7,3 +7,15 @@ theta = π/4
 c = [0., 0., 0., 1.]
 wavefront = W.(r,theta, (c,))
 plot(r, wavefront, Layout(title="Z_2^-2"))
+
+r = range(0.,1., 100)
+theta = range(0.,2π, 100)
+
+x = r.*sin.(theta')
+y = r.*cos.(theta')
+
+c = [0., 0., 0., 1.]
+wavefront = W.(r,theta', (c,))
+s = surface(z = wavefront, x=x, y=y)
+l = Layout(title="Z_2^-2")
+plot(s,l)
